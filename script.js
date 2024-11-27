e22a49f333989e7a09e215df8c81a0d1f4acd9a1
Adam MIR-SADJADI <117532525+ADMR-S@users.noreply.github.com>
2024-11-27 17:41:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
-var contexteAudio;
-document.addEventListener("click", () => {
-    if (!contexteAudio) {
-        contexteAudio = new AudioContext();
-    }
-});
+//IMPORT WAM BATTERIE : https://www.webaudiomodules.com/community/plugins/burns-audio/drumsampler/index.js
+const AudioContext = window.AudioContext // Default
+	|| window.webkitAudioContext 
+	|| false;
+const audioContext = new AudioContext();
+const mediaElementSource = audioContext.createMediaElementSource(player);
+
+// Very simple function to connect the plugin audionode to the host
+const connectPlugin = (audioNode) => {
+	mediaElementSource.connect(audioNode);
+	audioNode.connect(audioContext.destination);
+};
+const mountPlugin = (domNode) => {
+	mount.innerHtml = '';
+	mount.appendChild(domNode);
+};
+(async () => {
+	// Init WamEnv
+	const { default: initializeWamHost } = await import("https://www.webaudiomodules.com/sdk/2.0.0-alpha.6/src/initializeWamHost.js");
+	const [hostGroupId] = await initializeWamHost(audioContext);
+  // Import WAM
+	const { default: WAM } = await import('https://www.webaudiomodules.com/community/plugins/burns-audio/drumsampler/index.js');
+	// Create a new instance of the plugin
+	const instance = await WAM.createInstance(hostGroupId, audioContext);
+	window.instance = instance;
+  // Connect the audionode to the host
+	connectPlugin(instance.audioNode);
+  const pluginDomNode = await instance.createGui();
+
+	mountPlugin(pluginDomNode);
+  player.onplay = () => {
+		audioContext.resume(); // audio context must be resumed because browser restrictions
+	};
+})();
+
 
 const createScene = async function () {
     const scene = new BABYLON.Scene(engine);
@@ -15,15 +44,36 @@ const createScene = async function () {
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
-    const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-    sphere.position.y = 1;
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+    
+    // Créer le cylindre (élément de la batterie)
+    const cylinder = BABYLON.MeshBuilder.CreateCylinder("cylinder", { diameter: 1, height: 0.5 }, scene);
+    cylinder.position = new BABYLON.Vector3(0, 1, 0); // Ajuster la hauteur selon besoin
 
+    // Charger le son pour le coup
+    const drumSound = new BABYLON.Sound("drumSound", "sons/drum-hit.mp3", scene);
+
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
     const env = scene.createDefaultEnvironment();
+    // Ajouter le support XR
     const xr = await scene.createDefaultXRExperienceAsync({
         floorMeshes: [env.ground],
     });
-    
+
+    // Détection de collision avec les contrôleurs
+    xr.input.onControllerAddedObservable.add((controller) => {
+        const controllerMesh = BABYLON.MeshBuilder.CreateBox("controllerMesh", { size: 0.1 }, scene);
+        controllerMesh.parent = controller.grip;
+
+        // Vérifie la collision à chaque image
+        scene.registerBeforeRender(() => {
+            if (controllerMesh.intersectsMesh(cylinder, false)) {
+                console.log("Collision détectée avec le cylindre !");
+                drumSound.play(); // Jouer le son de batterie
+            }
+        });
+    });
+
+
     return scene;
 };
 
