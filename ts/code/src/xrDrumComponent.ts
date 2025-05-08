07ccf83aa5dcf06ed4078da0dc69b7bc30cc5c3f
Adam MIR-SADJADI <117532525+ADMR-S@users.noreply.github.com>
2025-05-08 13:52:32 +0200

    Delete ts/code/src/xrDrumComponent.ts

--- ts/code/src/xrDrumComponent.ts
+++
@@ -1,7 +0,0 @@
-interface XRDrumComponent {
-    playSoundOnTrigger(name: string, midiKey: number, duration: number) : void;
-    createDrumComponentBody(name: string, diameter: number, height: number) : void;
-    createDrumComponentTrigger(name: string, diameter: number, height: number) : void;
-}
-
-export default XRDrumComponent;
