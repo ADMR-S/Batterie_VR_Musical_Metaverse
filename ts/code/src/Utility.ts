12f05b4efa8656adb59af23fe23c109c5a0e2b5e
Adam MIR-SADJADI <mirsadjadi2011@yahoo.fr>
2025-05-12 08:34:59 +0200

    updated inspector call

--- ts/code/src/Utility.ts
+++ ts/code/src/Utility.ts
@@ -1,12 +1,27 @@
 import { Scene } from "@babylonjs/core/scene";
-import { Inspector } from "@babylonjs/inspector";
+//import { Inspector } from "@babylonjs/inspector";
 
 export class Utility {
     static setupInspectorControl(scene : Scene){
         window.addEventListener("keydown", (event: KeyboardEvent) => {
             if (event.key === "i") {
-                Inspector.Show(scene, {});
+             //Inspector.Show(scene, {});
+                scene.debugLayer.show({ embedMode: false }).then(function () {
+                    const sceneExplorerHost = document.getElementById("scene-explorer-host");
+                    const inspectorHost = document.getElementById("inspector-host");
+
+                    if (sceneExplorerHost) {
+                        sceneExplorerHost.style.zIndex = "1000";
+                        sceneExplorerHost.style.position = "fixed";
+                    }
+
+                    if (inspectorHost) {
+                         inspectorHost.style.zIndex = "1000";
+                        inspectorHost.style.position = "fixed";
+                    }
+                });
             }
         });
     }
 }
+         
--- ts/code/src/XRDrumKit.ts
+++ ts/code/src/XRDrumKit.ts
@@ -16,9 +16,10 @@ import XRLogger from "./XRLogger";
 
 //TODO : 
     //Rapport
-    //Remplacer les triggers havok par de réelles collisions avec les meshs appropriés. Voir pour collision entre baguettes et objets statiques
-    //importer stands, trône, fusionner XRDrum et XRCymbal
-    //Implementer autres drumComponents avec blender
+    //Tester vibrations collision controllers
+    //Ajuster échelle de la batterie (trop grande)
+    //Optimiser scène et modèle 3D pour éviter les ralentissements
+    //Remplacer les triggers havok par de réelles collisions avec les meshs appropriés ? (utile si les cymbales doivent bouger) -> Voir pour collision entre baguettes et objets statiques
     //Pourquoi le paramètre de vélocité sur scheduleEvent ne fonctionne pas ? (même avec valeurs manuelles)
     //Retour haptique et visuel collision baguettes (vibrations, tremblement du tambour, oscillation des cymbales...)
     //Ajouter option pour sortir un enregistrement sous forme de piano roll / liste d'évènements MIDI
@@ -28,9 +29,9 @@ import XRLogger from "./XRLogger";
     //Bien vérifier qu'on ne peut pas taper par dessous pour les tambours mais ok pour cymbales
     //Empêcher les objets de passer à travers le sol
     //Sons différents en bordure / au centre de la peau ? (+ bordure métallique)
-    //Grosse caisse / Hi-Hat ? Besoin d'une pédale (appuyer sur un bouton ?)
+    //Pédale Grosse caisse / Hi-Hat ? (appuyer sur un bouton en attendant d'ajouter des pédales midi ?)
     //Tenir les baguettes avec la gachette interne plutôt ? (permet d'avoir une autre position de main, plus adaptée ?)
-    //Replace invisible cube meshes for controllers by physicsImpostors
+    //Replace invisible cube meshes for controllers by physicsImpostors ?
     //Use a 0 distance constraint to snap drumsticks to hands ? 
     //Test interactions projet Ismail
 
