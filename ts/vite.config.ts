ca89ef3520f1ae0cf5a6a71d3457a418b348912d
Adam MIR-SADJADI <117532525+ADMR-S@users.noreply.github.com>
2025-11-10 21:43:51 +0100

    change build to deploy in https

--- ts/vite.config.ts
+++ ts/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
     optimizeDeps: {
         exclude: ['@babylonjs/havok'],
+    },
+    server: {
+        https: {
+            key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
+            cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt')),
+        },
+        host: true,
     },
 });
